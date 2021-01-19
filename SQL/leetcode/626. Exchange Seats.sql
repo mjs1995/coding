1109e220e920b5a3a6de79966b6355472ad6cbf2
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-01-19 14:45:06 +0900

    Create 626. Exchange Seats.sql

---
+++ SQL/leetcode/626. Exchange Seats.sql
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/exchange-seats/
+select (case when mod(id,2) = 1 and id != (select count(*) from seat) then id+1
+        when mod(id,2) = 0 then id-1
+        else id end) id, student
+from seat
+order by id asc;
