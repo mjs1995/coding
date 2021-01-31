5fc2d81b96f5c60802a4ed24d268f25f0aab3e26
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-01-31 15:27:27 +0900

    Update 소수의 판별.py

--- Etc/소수의 판별.py
+++ Etc/소수의 판별.py
@@ -13,3 +13,20 @@ def is_prime_number(x):
     return False
 print(is_prime_number(4))
 print(is_prime_number(7))
+
+
+#에라토스테네스의 알고리즘
+import math
+n = 1000
+array = [True for i in range(n+1)]
+
+for i in range(2, int(math.sqrt(n)) + 1):
+  if array[i] == True:
+    j = 2
+    while i * j <= n:
+      array[i * j] = False
+      j += 1
+for i in range(2, N+1):
+  if array[i]:
+    print(i, end= ' ')
+    
