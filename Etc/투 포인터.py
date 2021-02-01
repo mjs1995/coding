8bb653cd26fafe11d06843b1d74ea833bc6a826e
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-01 15:10:44 +0900

    Update 투 포인터.py

--- Etc/투 포인터.py
+++ Etc/투 포인터.py
@@ -37,3 +37,32 @@ while i < n or j < m :
 
 for i in result:
   print(i, end = ' ')
+  
+### 구간합
+n = 5
+data = [10,20,30,40,50]
+
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+  sum_value += i
+  prefix_sum.append(sum_value)
+  
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left-1])
+
+###순열
+import itertools
+
+data = [1,2]
+for x in itertools.permutations(data,2):
+  print(list(x))
+  
+###조합
+import itertools
+
+data = [1,2,3]
+
+for x in itertools.combinations(data,2):
+  print(list(x), end = ' ')
