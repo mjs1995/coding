86a37a59906d3812789fe52c4cd14f7c93d4e033
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-23 00:23:42 +0900

    Create 04_카드 정렬하기.py

---
+++ Sorting/04_카드 정렬하기.py
@@ -0,0 +1,20 @@
+import heapq
+
+n = int(input())
+
+heap = []
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap, data)
+    print(heap)
+    
+result = 0
+while len(heap) != 1:
+    one = heapq.heappop(heap)
+    two = heapq.heappop(heap)
+    
+    sum_value = one + two
+    result += sum_value
+    heapq.heappush(heap, sum_value)
+    
+print(result)
