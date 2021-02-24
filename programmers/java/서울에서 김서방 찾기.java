ced9b90cc64e42efef5afa7f432f98a3897c09eb
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-24 20:59:59 +0900

    Create 서울에서 김서방 찾기.java

---
+++ programmers/java/서울에서 김서방 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        int index = -1;
+        for (int i=0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                index = i;
+                break;
+            }
+        }
+        
+        answer = "김서방은 " + index + "에 있다";
+        return answer;
+    }
+}
