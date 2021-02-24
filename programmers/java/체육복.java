80559a4c10852c288998762eff19fcd3da5115f5
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-24 18:28:33 +0900

    Create 체육복.java

---
+++ programmers/java/체육복.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int lost1 = 0;
+        int count = 0;
+        
+        for(int i =0; i<lost.length; i++) {
+            for(int j=0; j<reserve.length; j++) {
+                if(lost[i] == reserve[j]) {
+                    lost1 ++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        for (int i=0; i<lost.length; i++) {
+            for(int j=0; j<reserve.length; j++) {
+                if(lost[i] == reserve[j] + 1 || lost[i] == reserve[j] - 1) {
+                    count++;
+                    reserve[j] = -1;
+                    break;
+                        
+                }
+            }
+        }
+              
+        answer = n - lost.length + lost1 + count;
+        return answer;
+    }
+}
