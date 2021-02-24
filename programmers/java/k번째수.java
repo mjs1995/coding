ba46e767fa271382c0b1a2be0a09993d806b8ee4
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-24 15:39:18 +0900

    Create k번째수.java

---
+++ programmers/java/k번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        ArrayList<Integer> arr = new ArrayList();
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                arr.add(array[j]);
+            }
+            Collections.sort(arr);
+            answer[i] = arr.get(commands[i][2] - 1);
+            arr.clear();
+        }
+        return answer;
+    }
+}
