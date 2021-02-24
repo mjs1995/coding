702f7484f63974e501234b91da1b5bdf3c7e7d8b
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-02-24 15:44:33 +0900

    Create 완주하지 못한 선수.java

---
+++ programmers/java/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        String temp = "";
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i = 0;
+        
+        while(i < completion.length) {
+            if(!completion[i].equals(participant[i])){
+                temp = participant[i];
+                break;
+            } else {
+                i++;
+            }
+        }
+        
+        if(!temp.equals("")){
+            answer = temp;
+        } else {
+            answer = participant[participant.length - 1];
+        }
+        return answer;
+    }
+}
