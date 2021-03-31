033f9c2c3a911e359f49f11312767aa35b5f5bdf
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-03-31 13:24:06 +0900

    Create 2609_최대공약수와 최소공배수.py

---
+++ BOJ/2609_최대공약수와 최소공배수.py
@@ -0,0 +1,14 @@
+a,b = map(int,input().split())
+
+def gcd(a, b) :
+    if b==0:
+        return a
+    else :
+        return gcd(b,a%b)
+
+def lcm(a, b) :
+    g = gcd(a,b)
+    return int(g*(a/g)*(b/g))
+
+print(gcd(a,b))
+print(lcm(a,b))
