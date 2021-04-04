621b32dc2833c00405c0d482b09c44351143767f
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-04-04 23:28:22 +0900

    Create Write a function.py

---
+++ HackerRank/Write a function.py
@@ -0,0 +1,23 @@
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+def is_leap(n):
+    if n % 400 == 0:
+        return True
+    if n % 100 == 0:
+        return False
+    if n % 4 == 0:
+        return True
+    return False
+
+print(is_leap(int(input())))
