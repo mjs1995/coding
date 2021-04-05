708438d202a0608353c96845e2ea330add6d0f2a
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-04-05 22:33:13 +0900

    Create Print Function.py

---
+++ HackerRank/Print Function.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = ''
+    for i in range(n):
+        a += str(i+1)
+    print(a)
+    
+n = int(input()) 
+for n in range(1,n+1):
+    print(n, end='') 
