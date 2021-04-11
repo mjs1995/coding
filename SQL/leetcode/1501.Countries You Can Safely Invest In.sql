a7eda6c3b84a7912e4b1b3b04e9b2c62910bbf1d
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-04-11 14:36:46 +0900

    Create 1501.Countries You Can Safely Invest In.sql

---
+++ SQL/leetcode/1501.Countries You Can Safely Invest In.sql
@@ -0,0 +1,5 @@
+select c2.name as country 
+from Calls c1, Person p, Country c2
+where (p.id=c1.caller_id or p.id=c1.callee_id) and c2.country_code=left(p.phone_number,3)
+group by c2.name 
+having avg(duration)>(select avg(duration) from Calls)
