58e38123d8b014bc6a1e917036ce339c2eb1ec60
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-04-16 14:56:38 +0900

    Create 11866_요세푸스 문제 0.py

---
+++ BOJ/11866_요세푸스 문제 0.py
@@ -0,0 +1,15 @@
+n,k = map(int,input().split())
+q = [i for i in range(1,n+1)]
+print("<", end= '')
+i = 0
+
+while len(q) > 1:
+    i = i+k
+    if i > len(q):
+        i = i % len(q)
+        if i == 0:
+            i = i +len(q)
+    i = i-1
+    print(str(q.pop(i)), end=", ")
+    print(q)
+print("{}>".format(str(q.pop())))
