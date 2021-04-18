c2e43f6f0aa13aaa3f11ff681bc3d854c4c5aad6
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-04-18 23:10:45 +0900

    Create 1920_수 찾기.py

---
+++ BOJ/1920_수 찾기.py
@@ -0,0 +1,23 @@
+def BinarySearch(arr, val, low, high):
+    if low > high:
+        return False
+    
+    mid = (low + high) // 2
+    if arr[mid] > val:
+        return BinarySearch(arr, val, low, mid - 1)
+    elif arr[mid] < val:
+        return BinarySearch(arr, val, mid + 1, high)
+    else:
+        return True
+
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+M_list = list(map(int, input().split()))
+A = sorted(A)     
+
+for m in M_list:
+    if BinarySearch(A, m, 0, N-1):
+        print(1)
+    else:
+        print(0)
