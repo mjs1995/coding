7b2865d60ec723bede4dba9691bd0256c2b18c96
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-05-18 15:03:29 +0900

    Create 올바른 괄호.py

---
+++ programmers/Python/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = []
+    
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
